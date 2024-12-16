2358a26f297b3a8b54373996bf00e71d80ff2cc3
sherlynkek <sherlynkek00@gmail.com>
2024-12-16 10:56:37 +0800

    first commit

---
+++ src/test/java/sg/edu/nus/iss/Mini/Project/MiniProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.Mini.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
