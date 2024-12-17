6bea2989a6f987a8bef7acd1b6f17404972fa8ad
sherlynkek <sherlynkek00@gmail.com>
2024-12-18 00:18:37 +0800

    user controller

--- src/main/java/sg/edu/nus/iss/Mini/Project/controller/UserController.java
+++ src/main/java/sg/edu/nus/iss/Mini/Project/controller/UserController.java
@@ -1,9 +1,15 @@
 package sg.edu.nus.iss.Mini.Project.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import sg.edu.nus.iss.Mini.Project.model.User;
 
@@ -13,16 +19,37 @@ import sg.edu.nus.iss.Mini.Project.model.User;
 @RequestMapping("/user")
 public class UserController {
     
+    private List<User> users = new ArrayList<>();
+
     @GetMapping("/register")
     public String showRegisterPage(Model model) {
-        
+        model.addAttribute("user", new User());
         return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@ModelAttribute("user") User user, Model model) {
+        users.add(user);
+        model.addAttribute("message", "Registration is successful");
+        return "login";
     }
 
     @GetMapping("/login")
     public String showLoginPage(Model model) {
-        model.addAttribute("user", new User());
         return "login";
     }
 
+    @PostMapping("/login")
+    public String loginUser(@RequestParam String username, 
+                            @RequestParam String password, Model model) {
+        
+        for(User user : users) {
+            if(user.getUsername().equals(username) && user.getPassword().equals(password)) {
+                model.addAttribute("message", "Login successful");
+                return "welcome";
+            }
+        }
+        model.addAttribute("message", "Invalid username or password");
+        return "login";
+    }
 }
