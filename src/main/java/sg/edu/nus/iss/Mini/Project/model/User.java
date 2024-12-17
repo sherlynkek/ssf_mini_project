a5030794ce5dcba28915e92d52ba73b5d4e78e09
sherlynkek <sherlynkek00@gmail.com>
2024-12-18 00:03:47 +0800

    user

--- src/main/java/sg/edu/nus/iss/Mini/Project/model/User.java
+++ src/main/java/sg/edu/nus/iss/Mini/Project/model/User.java
@@ -1,31 +1,18 @@
 package sg.edu.nus.iss.Mini.Project.model;
 
-import java.util.UUID;
 
 public class User {
-    private String id;
     private String username;
     private String password;
-    private String email;
 
 
-    public User(String id, String username, String password, String email) {
-        this.id = UUID.randomUUID().toString();
+    public User(String username, String password) {
         this.username = username;
         this.password = password;
-        this.email = email;
     }
 
     public User() {
 
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
     }
 
     public String getUsername() {
@@ -44,17 +31,9 @@ public class User {
         this.password = password;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     @Override
     public String toString() {
-        return id + "," + username + "," + password + "," + email;
+        return username + "," + password;
     }
     
 }
